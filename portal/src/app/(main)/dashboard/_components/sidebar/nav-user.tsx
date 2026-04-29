99a180b6f0c2d003e12086857aaab7f0a984c0a6
Michelle Argueta <michelleargueta401@gmail.com>
2026-04-29 10:02:29 -0600

    conexion boton mi perfil -> cuenta

--- portal/src/app/(main)/dashboard/_components/sidebar/nav-user.tsx
+++ portal/src/app/(main)/dashboard/_components/sidebar/nav-user.tsx
@@ -81,7 +81,7 @@ export function NavUser({
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
-              <DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push('/dashboard/perfil')}>
                 <CircleUser />
                 Cuenta
               </DropdownMenuItem>
